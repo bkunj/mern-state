d1a4f1c990980c18f49feefb090bc24adf74f864
bkunj <kunjb6789@gmail.com>
2023-12-02 12:00:47 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,23 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server: {
+  //    proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       secure: false,
+  //    },
+  //   },
+  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        //rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+
   plugins: [react()],
-})
+});
